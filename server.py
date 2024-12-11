5014e1b4a45f4675a7883eb957d2f0267523412b
Gitonga Bretton <65695871+bretton7700@users.noreply.github.com>
2024-12-11 14:02:22 +0300

    Update server.py
    runner

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def hello_world(request):
     name = os.environ.get("NAME")
     if name == None or len(name) == 0:
         name = "different branch"
-    message1 = "Good men, " + name + "!\n"
+    message1 = "Broke men finish last, " + name + "!\n"
     message = "uko sure men, " + name + "!\n"
     return Response(message)
 
